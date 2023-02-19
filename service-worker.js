ab9f0cd42540358b3ccf26fed22e1ada59edf3dd
yhien25 <113863862+yhien25@users.noreply.github.com>
2023-02-20 07:12:04 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "portfolio.github.io/precache-manifest.408bd0da57af79293d9e199ce62dd186.js"
+  "./precache-manifest.5c985c053919b0ccc1f3c33f68947f79.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("portfolio.github.io/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
