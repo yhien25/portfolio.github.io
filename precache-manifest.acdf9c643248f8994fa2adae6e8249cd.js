cdd09fc4aaa725c43a385891e57ebd47b2f74002
yhien25 <113863862+yhien25@users.noreply.github.com>
2023-02-20 06:58:09 +0800

    update

---
+++ precache-manifest.acdf9c643248f8994fa2adae6e8249cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "95cf3b4af39f80f309c6",
+    "url": "/static/css/main.85001ffb.chunk.css"
+  },
+  {
+    "revision": "95cf3b4af39f80f309c6",
+    "url": "/static/js/main.95cf3b4a.chunk.js"
+  },
+  {
+    "revision": "170ed560ba3bc50e80d5",
+    "url": "/static/js/1.170ed560.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "a14b1de52f62cac34a9c90851552bf2a",
+    "url": "/static/media/bg3.a14b1de5.jpg"
+  },
+  {
+    "revision": "55ddad3406723284761aa396464da29a",
+    "url": "/static/media/ebook.55ddad34.jpg"
+  },
+  {
+    "revision": "bfad127c8724a1ade416941ba04eeabb",
+    "url": "/static/media/S1.bfad127c.png"
+  },
+  {
+    "revision": "474a111f5e7d23a5f2095f6fe7818cc8",
+    "url": "/static/media/marketplace1.474a111f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "63cc3967e1ea9174e6b8a55798452894",
+    "url": "/index.html"
+  }
+];
